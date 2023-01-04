f44b6688084e62cd402dec6c0549381b154a4cbe
Ethan Rousseau <erousseau@ptc.com>
2023-01-04 19:03:55 +0000

    WIP

--- action-summary.bash
+++ action-summary.bash
@@ -7,6 +7,7 @@ sha=$(git rev-parse HEAD)
 shortSHA=${sha:0:12}
 {
     #echo "Published \`${GITHUB_REF_NAME}\` [onshape-ng-modules@${shortSHA}](https://github.com/${GITHUB_REPOSITORY}/commit/${sha}) as NPM version ${version}"
+    echo "OUTCOME: ${TEST_OUTCOME}"
     if [ $? -eq 0 ]
     then
         echo "## \`${GITHUB_WORKFLOW}\` Passed!"
