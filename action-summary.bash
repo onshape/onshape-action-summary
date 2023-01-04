de0b9952d03f3b7329c457d07076f9e95f9a4119
Ethan Rousseau <erousseau@ptc.com>
2023-01-04 19:41:35 +0000

    WIP

--- action-summary.bash
+++ action-summary.bash
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
 
-name=$(node -p "require('./package.json').name")
-version=$(node -p "require('./package.json').version")
+#name=$(node -p "require('./package.json').name")
+#version=$(node -p "require('./package.json').version")
 
-sha=$(git rev-parse HEAD)
-shortSHA=${sha:0:12}
+#sha=$(git rev-parse HEAD)
+#shortSHA=${sha:0:12}
 {
     #echo "Published \`${GITHUB_REF_NAME}\` [onshape-ng-modules@${shortSHA}](https://github.com/${GITHUB_REPOSITORY}/commit/${sha}) as NPM version ${version}"
     echo "## \`${GITHUB_WORKFLOW}\` Finished"
